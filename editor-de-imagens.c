600287166a3c0ef0ecf27096608c70b93ac3593b
ViniciusGCaetano <viny6888@gmail.com>
2023-03-15 22:48:21 -0300

    feat: adicionar leitura de imagem

--- editor-de-imagens.c
+++ editor-de-imagens.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "./funcoes/ler_imagem.h"
 
 int main()
 {
-    return 0;
+
 }
--- funcoes/ler_imagem.h
+++ funcoes/ler_imagem.h
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-int ler_imagem(char caminho[])
+unsigned char** ler_imagem(char caminho[])
 {
     FILE* ptr;
     ptr = fopen(caminho, "r"); // ler imagem no caminho selecionado
@@ -19,20 +18,31 @@ int ler_imagem(char caminho[])
 
     char largura[10], altura[10];
     fscanf(ptr, "%s %s", largura, altura);
-    unsigned char (*arr)[atoi(largura)] = calloc(atoi(altura), sizeof *arr); //cria matriz baseada nos parâmetros da segunda linha
+
+    int int_largura = atoi(largura);
+    int int_altura = atoi(altura);
+
+    unsigned char ** arr = (unsigned char**) malloc(int_altura * sizeof(unsigned char*)); //cria matriz baseada nos parâmetros da segunda linha
+    
+    for (int i = 0; i < int_altura; i++) {
+        arr[i] = (unsigned char*) malloc(int_largura* sizeof(unsigned char));   // alocando memória para as colunas
+    }
+    
+    
 
     fscanf(ptr, "%u ", &arr[0][0]); //Solução temporária: alocar o valor máximo de cinza na posição (0,0) da matriz para ser apagado em seguida
 
     for(int linha = 0; linha < atoi(altura); linha++){
         for(int coluna = 0; coluna < atoi(largura); coluna++){
             fscanf(ptr, "%u ", &arr[linha][coluna]); //preenchendo valores
-            printf("%u, ", arr[linha][coluna]);
+            // printf("%u, ", arr[linha][coluna]);
         }
         
-        printf("\n");
+        // printf("\n");
     }
     
     fclose(ptr);
 
-    return 0;
+    return arr;
 }
+
