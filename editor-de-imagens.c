e96de18a837dbc12d0e08f00b6265a5235f3528b
ViniciusGCaetano <viny6888@gmail.com>
2023-03-21 20:44:08 -0300

    WIP: Integrando funcionalidades ao menu

--- editor-de-imagens.c
+++ editor-de-imagens.c
@@ -1,58 +1,66 @@
 #include <stdlib.h>
 #include <stdio.h>
-//#include "funcoes.h"
+#include "funcoes.h"
 
 #define MAX_LIMIT 180
 
 void printFilterMenu()
 {
-  printf("\n");
-  printf("---> Filtros disponíveis <---\n");
-  printf("[1] Negativo: inverte as cores\n");
-  printf("[2] Espelhamento: inverte a imagem horizontalmente\n");
-  printf("[3] Borramento: diminui a nitidez da imagem\n");
-  printf("[4] Brightening: clareia a imagem\n");
-  printf("\n");
+    printf("\n");
+    printf("---> Filtros disponíveis <---\n");
+    printf("[1] Negativo: inverte as cores\n");
+    printf("[2] Espelhamento: inverte a imagem horizontalmente\n");
+    printf("[3] Borramento: diminui a nitidez da imagem\n");
+    printf("[4] Brightening: clareia a imagem\n");
+    printf("\n");
 }
 
 int main()
 {
-  printf("---> Bem vindo ao Editor de Imagens! <---\n\n");
+    ler_imagem("./imagens_teste/pgm/image1.pgm");
 
-  printf("Forneça o caminho para a imagem a ser editada: \n : ");
-  char image_path[MAX_LIMIT];
-  fgets(image_path, MAX_LIMIT, stdin);
+    printf("---> Bem vindo ao Editor de Imagens! <---\n\n");
 
-  //TODO: error handling caso seja um path inválido ou a imagem não exista
+    printf("Forneça o caminho para a imagem a ser editada: \n : ");
+    char image_path[MAX_LIMIT];
+    scanf("%s", &image_path);
+    struct imagem imagem_input = ler_imagem(image_path);
+    
+    //TODO: error handling caso seja um path inválido ou a imagem não exista
 
-  int valid_filter_selected = 0;
-  int selected_filter;
-  printFilterMenu();
-  while (!valid_filter_selected)
-  {
-    printf("Selecione o filtro desejado (ex: 2): ");
-    scanf("%d", &selected_filter);
-
-    if (selected_filter < 0 || selected_filter > 4)
+    int valid_filter_selected = 0;
+    int selected_filter;
+    printFilterMenu();
+    while (!valid_filter_selected)
     {
-      printf("Filtro escolhido não é válido!\n");
-      continue;
+        printf("Selecione o filtro desejado (ex: 2): ");
+        scanf("%d", &selected_filter);
+
+        if (selected_filter < 0 || selected_filter > 4)
+        {
+        printf("Filtro escolhido não é válido!\n");
+        continue;
+        }
+
+        valid_filter_selected = 1;
     }
 
-    valid_filter_selected = 1;
-  }
+    switch (selected_filter)
+    {
+        case 1: //Negativo
+            negativo(imagem_input);
+        case 2: //Espelhamento
+            espelhamento(imagem_input);
+        case 3: //Borramento
+        break;
+        case 4: //Brightening
+        break;
+    }
+    
+    printf("Forneça o caminho para salvar a imagem: \n");
+    char caminho_para_guardar[MAX_LIMIT];
+    scanf("%s", &caminho_para_guardar);
+    gravar_imagem(imagem_input, caminho_para_guardar);
 
-  switch (selected_filter)
-  {
-    case 1: //Negativo
-      break;
-    case 2: //Espelhamento
-      break;
-    case 3: //Borramento
-      break;
-    case 4: //Brightening
-      break;
-  }
-
-  return 0;
+    return 0;
 }
